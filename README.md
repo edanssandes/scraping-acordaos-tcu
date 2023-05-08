56f1222bf66ac1b5d754b831f6acf6987783e75d
Edans Sandes <edans.sandes@gmail.com>
2023-05-08 13:03:38 -0300

    Atualização dos acórdãos de 2022 e 2023.

--- README.md
+++ README.md
@@ -1,8 +1,47 @@
-# scraping-acordaos-tcu
+# Acordãos do Tribunal de Contas da União (TCU)
 
-Este script baixa os Acordãos do TCU a partir da pesquisa integrada do site deste órgão.
-Os acórdãos e seus metadados são salvos em formato json em um diretório de saída. 
+Este repositório contém uma série de acórdãos do TCU obtidos em formato json. 
+Os arquivos foram obtidos da pesquisa integrada do portal deste órgão por 
+meio de scraping.
 
-Os acórdãos mais recentes (desde o último commit) já estão listados neste repositório. 
-Caso deseja atualizar esse diretório, utilize o script de download novamente.
+O script que realiza o download dos arquivos também está disponível, 
+sendo que existe uma restrição na velocidade de download para evitar 
+sobrecarga nos servidores do órgão. Deste modo, utilize o script somente
+para atualizar os acórdãos faltantes, caso necessário.
 
+### Metadados dos arquivos:
+
+Para fins de exemplo, seguem as chaves e trechos dos valores (truncados) contidos
+no arquivo json relativo ao Acórdão 225/2023 do TCU-Plenário.
+
+```
+KEY                           ACORDAO-COMPLETO-2418652
+TIPO                          ACÓRDÃO
+TITULO                        ACÓRDÃO 225/2023 ATA 6/2023 - PLENÁRIO
+FAVORITO                      false
+NUMACORDAO                    225
+ANOACORDAO                    2023
+NUMATA                        6/2023
+ACORDAO                       <p class="tCU_-_Ac_-_ite…CU_-_Ac_-_item_9_-_§§"/>
+ASSUNTO                       Recurso de revisão contr…, no Estado da Paraíba.
+COLEGIADO                     Plenário
+DATASESSAO                    15/02/2023
+ENTIDADE                      Estado da Paraíba; Mini…al e Agrário (extinto).
+INTERESSADOS                  3. Interessados/Responsá…Braga (XXX.674.201-XX).
+PROC                          <a href="https://pesquis…lank">025.264/2013-3</a>
+QUORUM                        <br>13.1. Ministros pre…ta e Weder de Oliveira.
+RELATOR                       AROLDO CEDRAZ
+RELATORDELIBERACAORECORRIDA   Ministro Bruno Dantas.
+RELATORIO                     <p class="tCU_-_Epígrafe….</p><p class="footer"/>
+ADVOGADO                      Rougger Xavier Guerra J…a Lúcia Navarro Braga.
+REPRESENTANTEMP               Subprocurador-Geral Paulo Soares Bugarin.
+SITUACAO                      OFICIALIZADO
+SUMARIO                       TOMADA DE CONTAS ESPECIA…AOS DEMAIS RECORRENTES.
+TIPOPROCESSO                  TOMADA DE CONTAS ESPECIAL (TCE)
+UNIDADETECNICA                Secretaria de Recursos …as Especial (SecexTCE).
+VOTO                          <p class="tCU_-_Epígrafe…r</p><p class="footer"/>
+DTATUALIZACAO                 20230217
+URLARQUIVO                    https://contas.tcu.gov.b…ltaPagina=S&item0=814523
+URLARQUIVOPDF                 https://contas.tcu.gov.b…ltaPagina=S&item0=814523
+EXTENSAOARQUIVO               doc
+```
--- download_acordaos.py
+++ download_acordaos.py
@@ -5,6 +5,7 @@ import re
 import os
 import json
 import sys
+from time import time, sleep
 
 
 
@@ -14,9 +15,10 @@ DESCRICAO = 'Script de Download de acordãos do TCU'
 
 DEFAULT_DIRETORIO_ACORDAOS = 'acordaos-tcu'
 DEFAULT_QUANTIDADE_ACORDAOS = 1000
+DEFAULT_TAXA_DOWNLOAD = 30
 
 # Baixa um determinado acordao caso ele ainda não tenha sido baixado
-def baixar_acordao_completo(diretorio_acordaos, acordao):
+def baixar_acordao_completo(diretorio_acordaos, acordao, taxa_download):
     diretorio = os.path.join(diretorio_acordaos, acordao["colegiado"], acordao["anoAcordao"])
     arquivo = os.path.join(diretorio, f'{int(acordao["numeroAcordao"]):04d}.json')
 
@@ -24,6 +26,8 @@ def baixar_acordao_completo(diretorio_acordaos, acordao):
     if os.path.isfile(arquivo):
         print(f'[ OK ] {arquivo}')
         return
+
+    t0 = time() # Tempo de inicio usado para manter a taxa de download
 
     # Cria o diretório se ele não existir
     os.makedirs(diretorio, exist_ok=True)
@@ -47,7 +51,13 @@ def baixar_acordao_completo(diretorio_acordaos, acordao):
     with open(arquivo, 'w') as f:
         json.dump(documento, f)
 
+    # Respeitando a taxa de download
+    t1 = time()
+    segundos_jah_decorridos = t1 - t0
+    respeitar_taxa_download(taxa_download, segundos_jah_decorridos)
+
     print(f'[DONE] {arquivo}')
+
 
 
 def tratar_argumentos():
@@ -62,13 +72,31 @@ def tratar_argumentos():
                         metavar="DIR", type=str, default=DEFAULT_DIRETORIO_ACORDAOS,
                         help='diretório onde os arquivos dos acórdãos ficarão salvos.')
 
+    parser.add_argument('--inicio-acordaos',
+                        metavar="N", type=int, default=1,
+                        help='o script baixará os acórdãos a partir do Nº registro.')
+
     parser.add_argument('--quantidade-acordaos',
-                        metavar="N", type=str, default=DEFAULT_QUANTIDADE_ACORDAOS,
-                        help='o script baixará os últimos "N" acórdãos publicados.')
+                        metavar="N", type=int, default=DEFAULT_QUANTIDADE_ACORDAOS,
+                        help='o script baixará "N" acórdãos publicados.')
+
+    parser.add_argument('--taxa-download',
+                        metavar="N", type=int, default=DEFAULT_TAXA_DOWNLOAD,
+                        help=f'Limita o download a uma taxa de "N" acórdãos por minuto. Default: {DEFAULT_TAXA_DOWNLOAD}/min')
+
 
     args = parser.parse_args()
 
     return args
+
+def respeitar_taxa_download(taxa_download, segundos_jah_decorridos):
+    if taxa_download <= 0:
+        return
+    intervalo = 60.0/taxa_download  # intervalo mínimo entre as requisições (em segundos)
+    segundos_restantes = intervalo - segundos_jah_decorridos
+    if segundos_restantes > 0:
+        sleep(segundos_restantes)
+
 
 def main():
     """
@@ -79,13 +107,14 @@ def main():
     args = tratar_argumentos()
 
     # Lista os "N" últimos acórdãos
+    i = args.inicio_acordaos
     n = args.quantidade_acordaos
-    r = requests.get(f'https://dados-abertos.apps.tcu.gov.br/api/acordao/recupera-acordaos?inicio=1&quantidade={n}')
+    r = requests.get(f'https://dados-abertos.apps.tcu.gov.br/api/acordao/recupera-acordaos?inicio={i}&quantidade={n}')
     acordaos = r.json()
 
     # Loop com o download de todos os acórdãos obtidos no link anterior
     for acordao in acordaos:
-        baixar_acordao_completo(args.diretorio_acordaos, acordao)
+        baixar_acordao_completo(args.diretorio_acordaos, acordao, args.taxa_download)
 
 
 if __name__ == '__main__':
